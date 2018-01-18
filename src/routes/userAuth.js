b70f47c6d352c6e2d97c8aa62ad8f5ad021738ec
Andrew Dorof <dr10n9@gmail.com>
2018-01-18 18:08:36 +0200

    idk where is error

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -2,12 +2,13 @@ const router = require('express').Router();
 const Auth = require('../modules/auth');
 const User = require('../modules/user');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const cookieParser = require('cookie-parser');
 const session = require('express-session')
 const bodyParser = require('body-parser');
 
 passport.use(Auth.Strategy);
+passport.serializeUser(Auth.serializeUser);
+passport.deserializeUser(Auth.deserializeUser);
 router.use(cookieParser());
 router.use(session({
     secret: 'SEGReT$25_',
@@ -23,8 +24,9 @@ router.use(bodyParser.urlencoded({
 router.use(bodyParser.json());
 
 router.post('/login', (req, res) => {
-    console.log(req.body);
-    passport.authenticate(local, {
+    console.log('login req.body:', req.body);
+    
+    passport.authenticate('local', {
         successRedirect: '/sendSuccess',
         failureRedirect: '/sendFailure'
     })
@@ -33,20 +35,20 @@ router.post('/login', (req, res) => {
 //в клиенте отключить авторедирект и получить статус логина,
 //после чего вывести инфу об ошибке или залогинить его
 router.get('/sendSuccess', (req, res) => {
-    res.json({
+    console.log('success')
+    res.send({
         login: 'success'
     });
 })
 
 router.get('/sendFailure', (req, res) => {
     console.log('failureeee')
-    res.json({
+    res.send({
         login: 'error'
     });
 });
 
-passport.serializeUser(Auth.serializeUser);
-passport.deserializeUser(Auth.deserializeUser);
+
 
 router.get('/logout', Auth.checkAuth, (req, res) => {
     req.logout();

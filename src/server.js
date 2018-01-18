127733feca7715c0e22606d17ee34f1a7bf18cf3
Andrew Dorof <dr10n9@gmail.com>
2018-01-18 12:18:12 +0200

    sorry

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const app = require('express')();
 const port = process.env.PORT || 2323;
 const db = require('./modules/database');
 const info = require('./modules/info')
+const News = require('./modules/news')
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
-app.listen(port, (err)=>{
-    if(err) console.log(err);
+app.listen(port, (err) => {
+    if (err) console.log(err);
     console.log('server on', port);
 })

1d48b2940b3096f02a2067cf1b621d18622abd7d
Andrew Dorof <dr10n9@gmail.com>
2018-01-18 13:47:30 +0200

    npm modules updates

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const app = require('express')();
 const port = process.env.PORT || 2323;
+const bodyParser = require('body-parser');
 const db = require('./modules/database');
-const info = require('./modules/info')
-const News = require('./modules/news')
-
+const info = require('./modules/info');
+const User = require('./modules/user');
 app.set('view engine', 'ejs');
+
+
+app.get('/login', (req, res)=>{});
+
+app.post('/register', (req, res)=>{
+
+})
 
 app.listen(port, (err) => {
     if (err) console.log(err);
     console.log('server on', port);
 })
+

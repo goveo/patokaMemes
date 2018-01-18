edad8fa0fa781479996b06cf058e6cba0418affb
Andrew Dorof <dr10n9@gmail.com>
2018-01-18 02:13:49 +0200

    database connection done

--- src/modules/database.js
+++ src/modules/database.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const paginate = require('mongoose-paginate');
 require('dotenv').load();
-console.log(process.env.DB_LINK);
 mongoose.connect(process.env.DB_LINK, { useMongoClient: true }, (err) => {
     if (err) {
-        return console.log(err)
+        return console.log('err:', err)
     } else {
         return console.log('db connected');
     };

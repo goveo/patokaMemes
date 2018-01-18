16b9e85a36cf6bf106559313f1ddc1a664a8619d
goveo <yuriybrusentsov@gmail.com>
2018-01-18 15:29:43 +0200

    added login to vue model

--- public/javascripts/model.js
+++ public/javascripts/model.js
@@ -7,6 +7,12 @@ var app = new Vue({
         chooseMeme: function (event) {
             console.log($(event.currentTarget).attr('param_id'));
             console.log('need to change memes and push info to db');
+        },
+        login: function() {
+            let login = $("#usernameLoginInput").val();
+            let password = $("#usernamePasswordInput").val();
+            console.log('login : ', login);
+            console.log('password : ', password);   
         }
     },
     created: function () {
